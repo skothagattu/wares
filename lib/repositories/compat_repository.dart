2cb78a38e208ae0329539b69c40bc7b038ab57d6
skothagattu <skothagattu@controlmod.com>
2023-12-20 10:17:48 -0500

    changes IP

--- lib/repositories/compat_repository.dart
+++ lib/repositories/compat_repository.dart
@@ -10,7 +10,7 @@ abstract class ICompatRepository {
 }
 
 class CompatRepository implements ICompatRepository {
-  final _host = "http://10.89.2.137:86/Compat"; // Adjust with your actual API endpoint
+  final _host = "http://10.89.2.66:86/Compat"; // Adjust with your actual API endpoint
   final Map<String, String> _headers = {
     "Accept": "application/json",
     "content-type": "application/json",
